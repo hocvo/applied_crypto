aab3512501542d1c0e27ee973c6ded176a5f7235
Hoc Vo <chohocvo@gmail.com>
2022-09-05 23:57:29 -0700

    add extended euclidean logic

--- P4/Program.cs
+++ P4/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#define debug
+using System;
 using System.Numerics;
 
 namespace P4
@@ -6,6 +7,7 @@ namespace P4
     class RSA
     {
         static BigInteger e = 65537;
+        static BigInteger NEG_ONE = new BigInteger(-1);
         static void Main(string[] args)
         {
             string p_e = args[0];
@@ -19,20 +21,73 @@ namespace P4
             p = p - BigInteger.Parse(p_c);
             BigInteger q = BigInteger.Pow(2, Int32.Parse(q_e));
             q = q - BigInteger.Parse(q_c);
-            BigInteger test = InverseMod(new BigInteger(65538));
+            BigInteger modulus = p * q;
+            BigInteger phi = (p-1) * (q-1);
+            BigInteger d = InverseMod(phi, e, NEG_ONE, NEG_ONE, NEG_ONE, NEG_ONE, phi);
+            //BigInteger d = InverseMod(26, 15, NEG_ONE, NEG_ONE, NEG_ONE, NEG_ONE, 26);
+#if debug
+            Console.WriteLine(d);
+            Console.WriteLine("Is ed % phi = 1: {0}", (e*d%phi)==1);
+#endif
         }
         /**
          * Find the inverse mod of phi for e value.
+         * From: http://www-math.ucdenver.edu/~wcherowi/courses/m5410/exeucalg.html
+         * For the first two steps, the value of this number is given: p0 = 0 and p1 = 1.
+         * For the remainder of the steps, we recursively calculate pi = pi-2 - pi-1 * qi-2 (mod n).
+         * Continue this calculation for one step beyond the last step of the Euclidean algorithm.
          */
-        static BigInteger InverseMod(BigInteger phi)
+        // p1 = p(i-1), p2 = p(i-2) where i is the ith iteration
+        // need to keep 2 last quotients to calculate P(i). 
+        static BigInteger InverseMod(BigInteger dividend, BigInteger divisor, BigInteger q1, BigInteger q2,
+         BigInteger p1, BigInteger p2, BigInteger modulus)
         {
-            BigInteger p_0 = 0;
-            BigInteger p_1 = 0;
-            BigInteger q = phi / e;
-            BigInteger r = phi % e;
-            Console.WriteLine(q);
-            Console.WriteLine(r);
-            return q;
+#if debug
+            Console.WriteLine("q1,q2 = {0},{1}", q1, q2);
+#endif
+            if (divisor == 0) {
+                return calculateP(p1, p2, q2, modulus);
+            }
+            BigInteger q = dividend / divisor;
+            BigInteger remainder = dividend % divisor;
+            if (remainder < 0) {
+                remainder += divisor;
+            }
+#if debug
+            if (remainder == 1) {
+                Console.WriteLine("Found remainder of 1. Has inverse!");
+            }
+            Console.WriteLine("{0} = {1}({2}) + {3}", dividend, q, divisor, remainder);
+#endif
+            if (p2 == NEG_ONE) {// first iteration
+                p2 = 0;
+            }
+            else if (p1 == NEG_ONE) {// second iteration
+                p1 = 1;
+            }
+            else {
+                BigInteger p = calculateP(p1, p2, q2, modulus);
+                p2 = p1;
+                p1 = p;
+            }
+            q2 = q1;
+            q1 = q;
+            return InverseMod(divisor, remainder, q1, q2, p1, p2, modulus);
+        }
+        /**
+        * pi = pi-2 - pi-1 * qi-2 (mod n).
+        */
+        static BigInteger calculateP(BigInteger p1, BigInteger p2,
+         BigInteger q2, BigInteger modulus)
+        {
+            BigInteger p = p2 - (p1 * q2) % modulus;
+            if (p < 0) {
+                p += modulus;
+            }
+#if debug
+            Console.WriteLine("p = {0}-({1}*{2}) % {3} = {4}", p2, p1, q2, modulus, p);
+#endif
+            return p;
         }
     }
 }
